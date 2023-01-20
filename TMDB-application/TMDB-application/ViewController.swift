00f14dcb58230c72022ce0d8eedc6091d41dc4fa
Artem Borodin <parapro.kh@gmail.com>
2023-01-20 11:40:37 +0100

    Initial commit

---
+++ TMDB-application/TMDB-application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB-application
+//
+//  Created by Артем Бородин on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
